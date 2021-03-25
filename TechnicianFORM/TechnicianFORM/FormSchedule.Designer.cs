10ad42b253fa9757a286362f8958c6544b4ebcb3
LukeJuerg <576601@student.belgiumcampus.ac.za>
2021-03-25 09:45:38 +0200

    Still unfinished

--- TechnicianFORM/TechnicianFORM/FormSchedule.Designer.cs
+++ TechnicianFORM/TechnicianFORM/FormSchedule.Designer.cs
@@ -37,6 +37,7 @@ namespace TechnicianFORM
             this.label2 = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnComplete = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // FName
@@ -121,11 +122,21 @@ namespace TechnicianFORM
             this.btnComplete.UseVisualStyleBackColor = true;
             this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
             // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(245, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(500, 252);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
             // Technician
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnComplete);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.label2);
@@ -151,6 +162,7 @@ namespace TechnicianFORM
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
