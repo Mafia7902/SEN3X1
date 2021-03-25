3e277d257d51280346df5e33a3a19006e7fb41d6
LukeJuerg <576601@student.belgiumcampus.ac.za>
2021-03-25 09:39:36 +0200

    Technician Form (unfinished)

---
+++ TechnicianFORM/TechnicianFORM/FormSchedule.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace TechnicianFORM
+{
+    partial class Technician
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LName = new System.Windows.Forms.Label();
+            this.lblSpecialisation = new System.Windows.Forms.Label();
+            this.lblTicketNum = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FName
+            // 
+            this.FName.AutoSize = true;
+            this.FName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FName.Location = new System.Drawing.Point(12, 133);
+            this.FName.Name = "FName";
+            this.FName.Size = new System.Drawing.Size(187, 20);
+            this.FName.TabIndex = 0;
+            this.FName.Text = "Technician First Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Technician Schedule";
+            // 
+            // LName
+            // 
+            this.LName.AutoSize = true;
+            this.LName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LName.Location = new System.Drawing.Point(12, 184);
+            this.LName.Name = "LName";
+            this.LName.Size = new System.Drawing.Size(186, 20);
+            this.LName.TabIndex = 2;
+            this.LName.Text = "Technician Last Name";
+            // 
+            // lblSpecialisation
+            // 
+            this.lblSpecialisation.AutoSize = true;
+            this.lblSpecialisation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSpecialisation.Location = new System.Drawing.Point(12, 233);
+            this.lblSpecialisation.Name = "lblSpecialisation";
+            this.lblSpecialisation.Size = new System.Drawing.Size(121, 20);
+            this.lblSpecialisation.TabIndex = 3;
+            this.lblSpecialisation.Text = "Specialisation";
+            // 
+            // lblTicketNum
+            // 
+            this.lblTicketNum.AutoSize = true;
+            this.lblTicketNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTicketNum.Location = new System.Drawing.Point(12, 287);
+            this.lblTicketNum.Name = "lblTicketNum";
+            this.lblTicketNum.Size = new System.Drawing.Size(189, 20);
+            this.lblTicketNum.TabIndex = 4;
+            this.lblTicketNum.Text = "Current Ticket Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current Ticket Number";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(602, 405);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 33);
+            this.btnBack.TabIndex = 6;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComplete.Location = new System.Drawing.Point(473, 405);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(97, 33);
+            this.btnComplete.TabIndex = 7;
+            this.btnComplete.Text = "Log Completion";
+            this.btnComplete.UseVisualStyleBackColor = true;
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // Technician
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblTicketNum);
+            this.Controls.Add(this.lblSpecialisation);
+            this.Controls.Add(this.LName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FName);
+            this.Name = "Technician";
+            this.Text = "Technician";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label FName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LName;
+        private System.Windows.Forms.Label lblSpecialisation;
+        private System.Windows.Forms.Label lblTicketNum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnComplete;
+    }
+}
+
