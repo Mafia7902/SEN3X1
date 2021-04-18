f8e1824db17fdf5120680467540a4b1fb92b4435
LukeJuerg <576601@student.belgiumcampus.ac.za>
2021-04-18 21:02:43 +0200

    Minor Changes
    I made some changes in the forms such as spelling corrections and adding some more fields.

--- ClinetMaintance/ClinetMaintance/ClinetMaintace.Designer.cs
+++ ClinetMaintance/ClinetMaintance/ClinetMaintace.Designer.cs
@@ -37,18 +37,25 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.txtAddress = new System.Windows.Forms.TextBox();
-            this.dgvProblemDetails = new System.Windows.Forms.DataGridView();
             this.bntUpdate = new System.Windows.Forms.Button();
             this.bntDelete = new System.Windows.Forms.Button();
             this.bntCancel = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProblemDetails)).BeginInit();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(8, 9);
+            this.label2.Location = new System.Drawing.Point(12, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 16);
             this.label2.TabIndex = 1;
@@ -65,7 +72,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(8, 51);
+            this.label3.Location = new System.Drawing.Point(12, 51);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(53, 16);
             this.label3.TabIndex = 3;
@@ -73,7 +80,7 @@
             // 
             // txtPhone
             // 
-            this.txtPhone.Location = new System.Drawing.Point(11, 70);
+            this.txtPhone.Location = new System.Drawing.Point(12, 70);
             this.txtPhone.Name = "txtPhone";
             this.txtPhone.Size = new System.Drawing.Size(182, 20);
             this.txtPhone.TabIndex = 4;
@@ -82,7 +89,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(8, 93);
+            this.label4.Location = new System.Drawing.Point(12, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 16);
             this.label4.TabIndex = 5;
@@ -90,20 +97,21 @@
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(12, 112);
+            this.txtEmail.Location = new System.Drawing.Point(11, 112);
             this.txtEmail.Name = "txtEmail";
             this.txtEmail.Size = new System.Drawing.Size(182, 20);
             this.txtEmail.TabIndex = 6;
+            this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(8, 177);
+            this.label5.Location = new System.Drawing.Point(9, 354);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(102, 16);
             this.label5.TabIndex = 7;
-            this.label5.Text = "Thier problem ";
+            this.label5.Text = "Their problem ";
             // 
             // label6
             // 
@@ -111,9 +119,9 @@
             this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.Location = new System.Drawing.Point(12, 135);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 16);
+            this.label6.Size = new System.Drawing.Size(107, 16);
             this.label6.TabIndex = 9;
-            this.label6.Text = "Physical Address";
+            this.label6.Text = "Street Address";
             // 
             // txtAddress
             // 
@@ -123,17 +131,9 @@
             this.txtAddress.TabIndex = 10;
             this.txtAddress.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
-            // dgvProblemDetails
-            // 
-            this.dgvProblemDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProblemDetails.Location = new System.Drawing.Point(11, 196);
-            this.dgvProblemDetails.Name = "dgvProblemDetails";
-            this.dgvProblemDetails.Size = new System.Drawing.Size(384, 150);
-            this.dgvProblemDetails.TabIndex = 11;
-            // 
             // bntUpdate
             // 
-            this.bntUpdate.Location = new System.Drawing.Point(42, 421);
+            this.bntUpdate.Location = new System.Drawing.Point(39, 475);
             this.bntUpdate.Name = "bntUpdate";
             this.bntUpdate.Size = new System.Drawing.Size(75, 23);
             this.bntUpdate.TabIndex = 12;
@@ -142,7 +142,7 @@
             // 
             // bntDelete
             // 
-            this.bntDelete.Location = new System.Drawing.Point(160, 421);
+            this.bntDelete.Location = new System.Drawing.Point(177, 475);
             this.bntDelete.Name = "bntDelete";
             this.bntDelete.Size = new System.Drawing.Size(75, 23);
             this.bntDelete.TabIndex = 13;
@@ -151,22 +151,108 @@
             // 
             // bntCancel
             // 
-            this.bntCancel.Location = new System.Drawing.Point(277, 421);
+            this.bntCancel.Location = new System.Drawing.Point(305, 475);
             this.bntCancel.Name = "bntCancel";
             this.bntCancel.Size = new System.Drawing.Size(75, 23);
             this.bntCancel.TabIndex = 14;
             this.bntCancel.Text = "Cancel";
             this.bntCancel.UseVisualStyleBackColor = true;
+            this.bntCancel.Click += new System.EventHandler(this.bntCancel_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(11, 196);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Suburb";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 219);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 16);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Unit Number";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 238);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(182, 20);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 261);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 16);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Postal Code";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(11, 280);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(182, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 303);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 16);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Province";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 373);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(365, 65);
+            this.listView1.TabIndex = 22;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(11, 322);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(182, 20);
+            this.textBox4.TabIndex = 23;
             // 
             // ClinetMaintace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(407, 510);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.bntCancel);
             this.Controls.Add(this.bntDelete);
             this.Controls.Add(this.bntUpdate);
-            this.Controls.Add(this.dgvProblemDetails);
             this.Controls.Add(this.txtAddress);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -179,7 +265,6 @@
             this.Name = "ClinetMaintace";
             this.Text = "ClinetMaintace";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvProblemDetails)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,10 +281,18 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtAddress;
-        private System.Windows.Forms.DataGridView dgvProblemDetails;
         private System.Windows.Forms.Button bntUpdate;
         private System.Windows.Forms.Button bntDelete;
         private System.Windows.Forms.Button bntCancel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
