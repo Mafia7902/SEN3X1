5a4efc7d4a4ada6b3b7c6c44ecdc6d48db7a44e4
KevinVW <71546128+Mafia7902@users.noreply.github.com>
2021-04-28 00:14:45 +0200

    Assigning Ticket To Tech

---
+++ BackEnd/BusinessLogic/TicketAssigning.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace BackEnd.BusinessLogic
+{
+    enum ContractScore
+    {
+        D = 1,
+        C,
+        B,
+        A
+    }
+    class TicketAssigning
+    {
+        public void assignTicket(string ticketID)
+        {
+            DataHandler handler = new DataHandler();
+            string contractID = "";
+            int neededScore = 1;
+
+            DataTable contractType;
+
+            DataTable AvalibleTechnicians;
+
+            contractType = handler.SelectUncompletedTickets(ticketID);
+            foreach (DataRow row in contractType.Rows)
+            {
+                contractID = row["ContractID"].ToString();
+            }
+            switch (contractID)
+            {
+                case "A":
+                    {
+                        neededScore = (int)ContractScore.A;
+                        break;
+                    }
+                case "B":
+                    {
+                        neededScore = (int)ContractScore.B;
+                        break;
+                    }
+                case "C":
+                    {
+                        neededScore = (int)ContractScore.C;
+                        break;
+                    }
+                case "D":
+                    {
+                        neededScore = (int)ContractScore.D;
+                        break;
+                    }
+            }
+
+            AvalibleTechnicians = handler.SelectTicketsAndSatisfactionScore(neededScore);
+
+            DataRow employee = AvalibleTechnicians.Rows[0];
+
+            string empID = employee["EmpID"].ToString();
+
+            handler.InsertTechnicianSchedule(empID, ticketID);
+        }
+    }
+}
