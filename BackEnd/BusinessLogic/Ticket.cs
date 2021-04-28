337c766877e9b58a87820b647d59fcdb94223d51
Stef98 <stefferreira14@gmail.com>
2021-04-28 02:13:02 +0200

    Added ticket class

---
+++ BackEnd/BusinessLogic/Ticket.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class Ticket
+    {
+        public Ticket()
+        {
+
+        }
+        public string IDGenV2()
+        {
+            
+            string newID = "";
+            Random random = new Random();
+            do
+            {
+                newID += random.Next(10).ToString();
+            } while (newID.Length != 8);
+            char letter = (char)('a' + random.Next(0, 5));
+            newID = letter.ToString().ToUpper() + newID;
+            return newID;
+        }
+
+        public void TicketInsert(string ticketID, string callId, string problemeDesc)
+        {
+            DataHandler dh = new DataHandler();
+            dh.InsertTicket(ticketID, callId, problemeDesc);
+        }
+
+    }
+}
