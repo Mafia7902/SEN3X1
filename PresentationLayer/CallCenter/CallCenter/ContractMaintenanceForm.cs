1334b33b5b7c0d29f49dc60f930eb6af4ff06a05
BrendonTheBear <72017327+Krazy2K@users.noreply.github.com>
2021-04-29 12:24:13 +0200

    Added to forms client choice and contract maintenance

---
+++ PresentationLayer/CallCenter/CallCenter/ContractMaintenanceForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CallCenter
+{
+    public partial class ContractMaintenanceForm : Form
+    {
+        private string _email;
+        public ContractMaintenanceForm(string email)
+        {
+            _email = email;
+            InitializeComponent();
+        }
+
+        private void ContractMaintenanceForm_Shown(object sender, EventArgs e)
+        {
+            MessageBox.Show(_email);
+        }
+
+        private void bntCancel_Click(object sender, EventArgs e)
+        {
+            ClientChoiceForm choiceForm = new ClientChoiceForm(_email);
+            choiceForm.Show();
+            this.Close();
+        }
+    }
+}
