f9c766bbde8ff6375de2f7277766879478d00936
BrendonTheBear <72017327+Krazy2K@users.noreply.github.com>
2021-05-09 20:42:51 +0200

    Added contract maintenance form

---
+++ ContractMaintance/DeactivateContract.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContractMaintance
+{
+    public partial class DeactivateContract : Form
+    {
+        public DeactivateContract()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCancel_Click(object sender, EventArgs e)
+        {
+            ContractMaintance contractMaintance = new ContractMaintance();
+            contractMaintance.Show();
+            this.Close();
+        }
+
+        private void bntDeactivate_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == null)
+            {
+                MessageBox.Show("Please Enter The Contract ID");
+            }
+
+            else if (txtID.Text != null)
+            {
+
+            }
+        }
+    }
+}
