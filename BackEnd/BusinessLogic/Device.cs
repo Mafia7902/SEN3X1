f8d2317c134e82359a4d7e294c7ef6203e2adc41
BrendonTheBear <72017327+Krazy2K@users.noreply.github.com>
2021-05-11 10:19:54 +0200

    Created Device.cs

---
+++ BackEnd/BusinessLogic/Device.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class Device
+    {
+        private string deviceID;
+        private string manufacturer;
+        private string model;
+
+        public Device()
+        {
+
+        }
+        public Device(string deviceID, string manufacturer, string model)
+        {
+            this.deviceID = deviceID;
+            this.manufacturer = manufacturer;
+            this.model = model;
+            
+        }
+
+
+        public string DeviceID
+        {
+            get { return deviceID; }
+            set { deviceID = value; }
+        }
+
+        public string Maufacturere
+        {
+            get { return manufacturer; }
+            set { manufacturer = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public void deviceInsert(string deviceID, string manufacturer, string model)
+        {
+            DataHandler dh = new DataHandler();
+            dh.InsertDevice(deviceID, manufacturer, model);
+        }
+    }
+}
