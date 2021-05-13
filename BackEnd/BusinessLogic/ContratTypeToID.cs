a1c861eb95fec1bb3e77518665df1e1845f343ff
KevinVW <71546128+Mafia7902@users.noreply.github.com>
2021-05-13 09:30:38 +0200

    ContractTypeToID

---
+++ BackEnd/BusinessLogic/ContratTypeToID.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackEnd.BusinessLogic
+{
+    class ContratTypeToID
+    {
+        
+        public string ContractType(string contractType)
+        {
+            string contractID="";
+            switch (contractType)
+            {
+                case "Platinum":
+                    {
+                        contractID = "A";
+                        break;
+                    }
+                case "Gold":
+                    {
+                        contractID = "B";
+                        break;
+                    }
+                case "Silver":
+                    {
+                        contractID = "C";
+                        break;
+                    }
+                case "Bronze":
+                    {
+                        contractID = "D";
+                        break;
+                    }
+            }
+            return contractID;
+
+        }
+
+    }
+}
